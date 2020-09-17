2a909ec8e55c6df2f317aa6871dd78f4925af807
gulli20 <gunnlaugur20@ru.is>
2020-09-17 09:36:13 +0000

    Started the algorithm

---
+++ tile_traveler.py
@@ -0,0 +1,6 @@
+# Have player position pos_row and pos_col  start 1,1
+# rows = 3 columns = 3
+# func moves(column, row) and returns where he can move
+    # if sentence to retrun the right moves (print)
+# func move(direction) and call func moves with new position (player pos_row pos_col)
+# when player enters pos_col = 3 and pos_row= 1
